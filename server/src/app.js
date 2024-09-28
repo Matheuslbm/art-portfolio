c857f691d87576c4bff03b0c4c1a9f718887cb81
Matheus lima <matheusslimabm@gmail.com>
2024-09-27 22:51:55 -0300

    server simples finalizado

--- server/src/app.js
+++ server/src/app.js
@@ -2,23 +2,30 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import routes from './routes/index.js'
+import path from 'node:path'
 
 dotenv.config()
 
 const app = express()
 
+// Habilita CORS e JSON
 app.use(cors())
 app.use(express.json())
-app.use('/api', routes)
 
-//Erro 404
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Endpoint não encontrado ' })
-})
+// Rotas de API
+app.use('/', routes)
+app.use('/obras', routes)
+app.use('/exposicoes', routes)
+app.use('/perfil', routes)
+app.use('/contato', routes)
 
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).json({ message: 'Erro no servidor' })
+// Servindo arquivos estáticos (imagens)
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname, 'src/public/images')))
+
+// Tratamento de Erro 404
+app.use((req, res) => {
+  res.status(404).json({ message: 'Página não encontrada' })
 })
 
 export default app
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,10 +1,33 @@
 import express from 'express'
 
+const router = express.Router()
 
-const router = express.Router();
-
+// Rota Home
 router.get('/', (req, res) => {
-    res.send('Hello World');
-  });
+  res.json({ message: 'Bem vindo a home!' })
+})
+
+// Rota Obras
+router.get('/obras', (req, res) => {
+    const obras = [
+        {title: 'Obra 1'}
+    ]
+    res.json({ message: 'Aqui estão as obras!' });
+})
+
+// Rota exposicoes
+router.get('/exposicoes', (req, res) => {
+    res.json({ message: 'Informações sobre exposições!' });
+})
+
+// Rota perfil (sobre mim)
+router.get('/perfil', (req, res) => {
+    res.json({ message: 'Perfil do artista!' });
+})
+
+// Rota contato
+router.get('/contato', (req, res) => {
+    res.json({ message: 'Formas de contato!' });
+})
 
 export default router
